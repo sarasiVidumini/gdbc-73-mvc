f4bce22b6683bc026a8948ae96688ae37d28d115
Sarasi Vidumini <sarasividumini13@gmail.com>
2025-03-27 13:54:03 +0530

    dbconnection is completed

---
+++ src/edu/ijse/mvc/db/DBConnection.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.db;
+
+/**
+ *
+ * @author sarasi-vidumini
+ */
+public class DBConnection {
+     private static DBConnection dBConnection;
+     
+     private Connection connection;
+     
+     private DBConnection() throws ClassNotFoundException, SQLException{
+         Class.forName("com.mysql.cj.jdbc.Driver");
+         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "password");
+     }
+     
+     public static DBConnection getInstance()  throws ClassNotFoundException, SQLException{
+         if(dBConnection == null){
+             dBConnection = new DBConnection();
+         }
+         return dBConnection;
+     }
+     
+     public Connection getConnection(){
+         return connection;
+     }
+ }
